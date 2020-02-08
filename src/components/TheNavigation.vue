d90d9d3189fd010ebfa76ffef1b44a85322529c7
Augusto Borges <gtoborges@gmail.com>
2020-02-08 15:09:59 -0300

    vue-travel-app

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="nav">
+    <p class="logo">Traveling the world</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li v-for="destination in destinations" 
+      :key="destination.name"
+      class="links">
+        <router-link 
+        :to="{
+          name: 'DestinationDetails',
+          params: { slug: destination.slug }
+        }"
+        >
+          {{ destination.name }}
+        </router-link>
+
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import store from "../store.js"
+export default {
+  data () {
+    return {
+      destinations: store.destinations
+    }
+  }
+}
+</script>
+<style scoped>
+#nav {
+  display: flex;
+}
+
+.nav-links {
+  display: flex;
+}
+
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+}
+
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+#nav a.navigate-active-link {
+  color: #ab26ab;
+}
+/* #nav a.router-link-exact-active {
+  color: #42b983;
+} */
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+// import Brazil from "../views/Brazil";
+// import Jamaica from "../views/Jamaica";
+// import Hawaii from "../views/Hawaii";
+// import Panama from "../views/Panama";
 
 Vue.use(VueRouter);
 
@@ -8,7 +12,8 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    props: true
   },
   {
     path: "/about",
@@ -18,10 +23,38 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  // {
+  //   path: "/brazil",
+  //   name: "brazil",
+  //   component: () => import(/* webpackChunkName: "brazil" */ "../views/Brazil")
+  // },
+  // {
+  //   path: "/hawaii",
+  //   name: "hawaii",
+  //   component: () => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii")
+  // },
+  // {
+  //   path: "/jamaica",
+  //   name: "jamaica",
+  //   component: () => import(/* webpackChunkName: "jamaica" */ "../views/Jamaica")
+  // },
+  // {
+  //   path: "/panama",
+  //   name: "panama",
+  //   component: () => import(/* webpackChunkName: "panama" */ "../views/Panama")
+  // },
+  {
+    path: "/details/:slug",
+    name: "DestinationDetails",
+    props: true,
+    component: () => import(/* webpackChunkName: "DestinationDetails */ "../views/DestinationDetails")
   }
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "navigate-active-link",
+  mode: "history",
   routes
 });
 
